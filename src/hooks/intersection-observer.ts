dff8ab58d34e2c6233ee707f477a035e1843cf42
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2025-10-27 19:58:47 +0000

    Added intersection observer

---
+++ src/hooks/intersection-observer.ts
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from "react";
+
+const useIntersectionObserver = <T extends HTMLElement>(onIntersecting: () => void) => {
+  const elementRefObj = useRef<T | null>(null);
+
+  useEffect(() => {
+    if (!elementRefObj.current) return;
+
+    const obs = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          onIntersecting();
+        }
+      });
+    });
+
+    obs.observe(elementRefObj.current);
+
+    return () => {
+      if (elementRefObj.current) {
+        obs.unobserve(elementRefObj.current);
+      }
+
+      obs.disconnect();
+    };
+  }, [elementRefObj]);
+
+  return { elementRefObj };
+};
+
+export default useIntersectionObserver;
